3b140504d09f793c5953905db85e86759171388b
kristyl26 <kristy2.leung@live.uwe.ac.uk>
2022-03-11 22:18:01 +0000

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,25 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function openForm(){
+	document.getElementById("myForm").style.display="block";
+}
+
+function closeForm(){
+	document.getElementById("myForm").style.display="none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
 (function($) {
 
 	var	$window = $(window),
